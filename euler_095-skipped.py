8841c47a3cd569fdabd8fefda83d0ad7eedfa180
Christopher Chen <christopher.chen1995@gmail.com>
2019-02-23 01:45:03 -0800

    Solve 95, 3min 52s runtime on my laptop

--- euler_095-skipped.py
+++ euler_095-skipped.py
@@ -19,4 +19,57 @@ Find the smallest member of the longest amicable chain with no element
 exceeding one million.
 """
 
+from utils import divisors
 
+
+def proper_divisors(n):
+    # import pudb;pudb.set_trace()
+    ds = set(divisors(n)) - {n}
+    return ds
+
+
+def divisor_sum(n):
+    return sum(proper_divisors(n))
+
+
+def chain(n):
+    found = set()
+    while True:
+        yield n
+        dsn = set(divisors(n)) - {n}
+        if n > 1000000:
+            raise ValueError('Too large!')
+        # print(n, len(dsn))
+        n = sum(dsn)
+        if n in found:
+            break
+        found.add(n)
+
+# print(divisors(24))
+# print(divisors(12496))
+# print(sum(divisors(12496)))
+print(list(chain(12496)))
+# quit()
+
+
+skip = set()
+chains = list()
+longest = list()
+for i in range(1, 1000000):
+    if i % 100000 == 0 or i == 12496:
+        print(i)
+    if i in skip:
+        continue
+    # print(i)
+    try:
+        mychain = list(chain(i))
+        chains.append(mychain)
+        # print(mychain)
+        if mychain[0] == mychain[-1] or i == 12496:
+            print(i,mychain)
+            if len(mychain) > len(longest):
+                longest = mychain
+    except ValueError:
+        pass
+
+print(min(longest))

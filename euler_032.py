82ea4e1f6a3b44ac75d5c05bf5f5a2c479485a70
Christopher Chen <christopher.chen1995@gmail.com>
2019-02-28 19:08:18 -0800

    Solve 32

--- euler_032.py
+++ euler_032.py
@@ -16,21 +16,61 @@ HINT: Some products can be obtained in more than one way so be sure to
 only include it once in your sum.
 """
 
-from math import log10
-from itertools import product
+from itertools import permutations
+import sys
 
 
 def generate_exponents():
-    for exp1 in range(1, 9 + 1):
-        for exp2 in range(9 - exp1):
-            for exp3 in range(9 - (exp1 + exp2)):
-            	if exp3 == exp1 + exp2:
-	                yield (exp1, exp2, exp3)
+    digits = 9
+    ds = [1, 2, 3, 4]
+    for exp1 in ds:
+        for exp2 in ds:
+            exp3 = digits - (exp1 + exp2)
+            if sum([exp1,exp2,exp3]) == digits:
+                yield (exp1, exp2, exp3)
+    return 
+    for exp1 in range(1, digits):
+        for exp2 in range(1, digits - exp1):
+            exp3 = digits - (exp1 + exp2)
+            if exp1 + exp2 - 1 > exp3:
+                break
+            if exp1 + exp2 < exp3:
+                continue
+            if abs((exp1 + exp2) - exp3) > 1:
+                break
+
+            # if exp3 <= 0:
+            #     break
+            # if exp1 + exp2 - 1 > exp3:
+            #     break
+
+            yield (exp1, exp2, exp3)
+
+
+def generate_pandigitals(start=1, stop=10):
+    for i in range(start, stop):
+        yield from permutations(set(range(1, i + 1)))
 
 
 def main():
-    for exps in generate_exponents():
-        print(exps)
+    if 'debug' in sys.argv:
+        for exps in generate_exponents():
+            print(exps)
+    found = set()
+    for pan in generate_pandigitals(start=9, stop=10):
+        for exps in generate_exponents():
+            a = int(''.join(map(str, pan[:exps[0]])))
+            b = int(''.join(map(str, pan[exps[0]:exps[0] + exps[1]])))
+            c = int(''.join(map(str, pan[exps[0] + exps[1]:])))
+            if a * b == c:
+                if 'debug' in sys.argv:
+                    print(a, b, c)
+                found.add(c)
+    if 'debug' in sys.argv:
+        print('Done solving')
+
+    # discount duplicates
+    print(sum(found))
 
 
 if __name__ == '__main__':

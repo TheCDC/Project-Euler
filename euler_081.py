e3cd1e7da569ceba6b2603edb779bd92592546ab
Christopher Chen <chris.chen@nswccorona.cloud>
2019-03-07 13:45:52 -0800

    Read input

--- euler_081.py
+++ euler_081.py
@@ -14,3 +14,20 @@ In the 5 by 5 matrix below, the minimal path sum from the top left to the bottom
 
 Find the minimal path sum, in matrix.txt (right click and "Save Link/Target As..."), a 31K text file containing a 80 by 80 matrix, from the top left to the bottom right by only moving right and down.
 """
+
+
+def get_diagonals(matrix):
+    pass
+
+
+def main():
+    with open('resources/p081_matrix.txt') as f:
+        s = f.read()
+    matrix = list()
+    matrix = [list(map(int, line.split(',')))
+              for line in s.split('\n') if len(line.strip()) > 0]
+    print(matrix)
+
+
+if __name__ == '__main__':
+    main()

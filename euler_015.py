fb59cb184c7a38d080fc3672a888e6f76bfab15c
Christopher Chen <chris.chen@nswccorona.cloud>
2019-04-22 13:07:17 -0700

    Some debugging stuff

--- euler_015.py
+++ euler_015.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 memory = dict()
-def pascal(x,y):
+def pascal(x,y,depth=0):
     fs = frozenset([x,y])
     if fs in memory:
         return memory[fs]
     if x == 1 or y == 1:
-        return 1
-    ret = pascal(x-1,y) + pascal(x,y-1)
+        ret =  1
+    else:
+        ret = pascal(x-1,y,depth+1) + pascal(x,y-1,depth+1)
+    # print(depth*'-',x,y)
     memory.update({fs:ret})
     return ret
 

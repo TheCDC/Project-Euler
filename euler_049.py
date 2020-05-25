66a826e9a67e5dd6ab45ee96a76642ca8e0ff30d
Christopher Chen <christopher.chen1995@gmail.com>
2020-05-24 22:58:03 -0700

    Solve 49 on first submission!

--- euler_049.py
+++ euler_049.py
@@ -14,5 +14,49 @@ increasing sequence.
 What 12-digit number do you form by concatenating the three terms in this
 sequence?
 """
+from utils import prime_sieve
+from collections import Counter
+from itertools import combinations
 
 
+def count_permutations():
+    # start by generating primes
+    mymap = dict()
+
+    for p in prime_sieve(10000):
+        # minimum digits
+        if len(str(p)) < 4:
+            continue
+        # associate all primes with the same multisets of digits.
+        key = ''.join(sorted(str(p)))
+        if key in mymap:
+            mymap[key].append(p)
+        else:
+            mymap.update({key: [p]})
+
+    for k in mymap:
+        # Take primes the equal multisets of digits three at a time to find the second 3-sequence.
+        # Some 'families' have more than 3 members so take combinations three at a time
+        # and check for a constant delta between those primes.
+        for comb in combinations(mymap[k], 3):
+
+            s = list(sorted(comb))
+
+            delta = s[1] - s[0]
+            for i in range(1, len(s)):
+                if s[i] - s[i-1] != delta:
+                    break
+            else:
+                yield s
+
+
+def main():
+    known = set([1487, 4817, 8147])
+    ps = list(count_permutations())
+    for p in ps:
+        if set(p) != known:
+            print(''.join(map(str, p)))
+
+
+if __name__ == '__main__':
+    main()

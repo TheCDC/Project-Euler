76e14199be8c46c0dd476674cb073cdc1b3a0fa7
Christopher Chen <christopher.chen1995@gmail.com>
2021-02-06 23:51:03 -0800

    Fix workspace.

--- euler/solutions/euler_038.py
+++ euler/solutions/euler_038.py
@@ -27,37 +27,38 @@ the concatenated product of an integer with (1,2, ... , n) where n > 1?
 
 
 from itertools import combinations, permutations
-from utils import generate_pandigitals
+from euler.solutions.utils import generate_pandigitals
+
 PANDIGITAL_SET = set(range(1, 10))
 
 
 def is_match(x, n):
-    digits = set(''.join(str(x*i) for i in range(1, n+1)))
+    digits = set("".join(str(x * i) for i in range(1, n + 1)))
     return len(digits) == 9
 
 
 def x_satisfies(x):
-    return any(is_match(x, n) for n in range(2, 9+1))
+    return any(is_match(x, n) for n in range(2, 9 + 1))
 
 
 def generate_xs():
     for ds in permutations(list(range(1, 10)), 4):
-        yield int(''.join(map(str, ds)))
+        yield int("".join(map(str, ds)))
 
 
 def pandigital_satisfies(p) -> bool:
     """is there an x that generates this pandigital with any n>1?"""
-    for n in range(2, 9+1):
-        parts = [p/z for z in range(1, n+1)]
+    for n in range(2, 9 + 1):
+        parts = [p / z for z in range(1, n + 1)]
         # parts must be all the same integers
-        if (len(set(parts)) == 1 ):
+        if len(set(parts)) == 1:
             return n
     return 0
 
 
 m = 0
 for p in generate_pandigitals(9):
-    p = int(''.join(map(str, p)))
+    p = int("".join(map(str, p)))
     s = pandigital_satisfies(p)
     if s > 0:
         m = max(m, p)
